93535119a1224ea0c348ecc5e55d55936f2ab26f
schmelto <tom.schmelzer@web.de>
2020-12-19 17:00:01 +0100

    write start page resolve #11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/top',
+    redirectTo: 'folder/top-headlines',
     pathMatch: 'full'
   },
   {
