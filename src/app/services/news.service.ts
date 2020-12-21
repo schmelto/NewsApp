f163b2ca23ac8849b4a3c887417305693faddead
schmelto <tom.schmelzer@web.de>
2020-12-21 14:24:29 +0100

    format

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -15,9 +15,7 @@ export class NewsService {
   constructor(private http: HttpClient) { }
 
   getData(type, country, category, search, page, language, from, to, sortBy) {
-    console.log(`${API_URL}/${type}?pageSize=5&page=${page}&q=${search}&language${language}&from=${from}&to=${to}&sortBy=${sortBy}&apiKey=${API_KEY}`)
     if (type == 'top-headlines') return this.http.get(`${API_URL}/${type}?pageSize=5&page=${page}&country=${country}&category=${category}&apiKey=${API_KEY}`);
     else return this.http.get(`${API_URL}/${type}?pageSize=5&page=${page}&q=${search}&language=${language}&from=${from}&to=${to}&sortBy=${sortBy}&apiKey=${API_KEY}`);     
-
   }
 }
