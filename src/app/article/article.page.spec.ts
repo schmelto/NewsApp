012a7ba873fa234c992e5f643f2ddb2d2a187a56
schmelto <tom.schmelzer@web.de>
2020-12-22 14:32:43 +0100

    ng update

--- src/app/article/article.page.spec.ts
+++ src/app/article/article.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ArticlePage } from './article.page';
@@ -7,7 +7,7 @@ describe('ArticlePage', () => {
   let component: ArticlePage;
   let fixture: ComponentFixture<ArticlePage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ArticlePage ],
       imports: [IonicModule.forRoot()]
